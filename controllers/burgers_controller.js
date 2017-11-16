4c7878f2bfbc3a8e25746234f1357dad08959adb
unknown <Valarie.Regas@gmail.com>
2017-11-16 15:59:22 -0500

    debugging

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,23 +2,23 @@ var express = require('express');
 
 var router = express.Router();
 
-var burgerJ = require('../models/burger.js');
+var burger = require('../models/burger.js');
 
 
 router.get('/', (req, res) => {
-    burgerJ.getAll((data) => {
+    burger.getAll((data) => {
         res.render('index', {burgers: data});
     });
 });
 
 router.get('/api/burgers', (req, res) => {
-    burgerJ.getAll((data) => {
+    burger.getAll((data) => {
     res.json(data);
     });
 });
 
 router.post('/api/devoured/:id', (req, res) => {
-    burgerJ.update('devoured', true, req.params.id, (data) => {
+    burger.update('devoured', true, req.params.id, (data) => {
     res.send(`${req.params.id}`);
     });
 });
