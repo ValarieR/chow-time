04bcef64a3b6392a76e72d1402f089ea5486d01b
unknown <Valarie.Regas@gmail.com>
2017-11-18 09:48:07 -0500

    further debugging

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,41 @@
-$(".devour-burger").on('click', function () {
-    
-})
+$(function() {
+    $(".devour-burger").on('click', function () {
+        var id = $(this).data('id');
+        var isDevoured = {
+            devoured: 1
+        };
+
+        console.log(id);
+
+        $.AJAX("/api/burgers/" + id, {
+            method: "PUT",
+            data: isDevoured
+        }).then(
+            function () {
+                location.reload();
+                console.log("dev change");
+            });
+    });
+
+    $('#add-burger').on('click', function(){
+        var newBurger = {
+            name: $('#text-area').val().trim(),
+            devoured: 0
+        };
+        $.ajax(`/api/burger`, {
+            method: 'POST',
+            data: newBurger
+        }).then(function(){
+            location.reload();
+        });
+    });
+
+    $('.delete-btn').on('click', function(){
+        var id = $(this).data('id');
+        $.ajax('/api/burger/' + id, {
+            method: 'DELETE'
+        }).then(function(){
+            location.reload();
+        });
+    });
+});
